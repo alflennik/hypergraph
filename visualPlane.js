d8b05d20c26fe9ca9c6545881e96433253425055
Paul-Clue <paulclue20@gmail.com>
2026-03-24 13:09:07 -0500

    Add drawing event listeners

--- visualPlane.js
+++ visualPlane.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("plane-1");
-var ctx = "";
+let ctx = "";
+let isDrawing = false;
 
 if (canvas.getContext) {
   ctx = canvas.getContext("2d");
@@ -8,4 +9,25 @@ if (canvas.getContext) {
   alert("Your browser does not support the canvas element.");
 }
 
-draw();
+canvas.addEventListener("mousedown", function(event) {
+  isDrawing = true;
+
+  const rect = canvas.getBoundingClientRect();
+  ctx.beginPath();
+  ctx.moveTo(event.clientX - rect.left, event.clientY - rect.top);
+});
+
+canvas.addEventListener("mousemove", function(event) {
+  if (!isDrawing) return;
+
+  const rect = canvas.getBoundingClientRect();
+  ctx.lineTo(event.clientX - rect.left, event.clientY - rect.top);
+  ctx.stroke();
+});
+
+canvas.addEventListener("mouseup", function() {
+  isDrawing = false;
+});
+
+
+// function draw();
