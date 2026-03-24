be9cf9f1c55853f1cee8dce42aa544255ab8f7ee
Paul-Clue <paulclue20@gmail.com>
2026-03-24 15:25:49 -0500

    Add straight line mouse move

--- visualPlane.js
+++ visualPlane.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById("plane-1");
 let ctx = "";
 let isDrawing = false;
+let startX = "";
+let startY = "";
+let snapshot = "";
 
 if (canvas.getContext) {
   ctx = canvas.getContext("2d");
@@ -13,15 +16,34 @@ canvas.addEventListener("mousedown", function(event) {
   isDrawing = true;
 
   const rect = canvas.getBoundingClientRect();
-  ctx.beginPath();
-  ctx.moveTo(event.clientX - rect.left, event.clientY - rect.top);
+
+  // Free Hand:
+  // ctx.beginPath();
+  // ctx.moveTo(event.clientX - rect.left, event.clientY - rect.top);
+
+  startX = event.clientX - rect.left;
+  startY = event.clientY - rect.top;
+
+  snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
 });
 
 canvas.addEventListener("mousemove", function(event) {
   if (!isDrawing) return;
 
   const rect = canvas.getBoundingClientRect();
-  ctx.lineTo(event.clientX - rect.left, event.clientY - rect.top);
+
+  // Free Hand:
+  // ctx.lineTo(event.clientX - rect.left, event.clientY - rect.top);
+  // ctx.stroke();
+
+  const currentX = event.clientX - rect.left;
+  const currentY = event.clientY - rect.top;
+
+  ctx.putImageData(snapshot, 0, 0);
+
+  ctx.beginPath();
+  ctx.moveTo(startX, startY);
+  ctx.lineTo(currentX, currentY);
   ctx.stroke();
 });
 
