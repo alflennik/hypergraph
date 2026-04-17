207e20ffcd59872b5039dd68eafffaa9572d4c0b
Paul-Clue <paulclue20@gmail.com>
2026-04-17 00:24:15 -0500

    Add canvas error handling

--- visualizationPlane2/visualizationPlane_2.js
+++ visualizationPlane2/visualizationPlane_2.js
@@ -4,13 +4,34 @@ import createGraph from '../hypergraph.js';
 
 const graph = createGraph();
 
-(() => {
-  const canvas = document.getElementById("plane-1");
-  const ctx = canvas.getContext("2d");
+const canvas = document.getElementById("plane-1");
+let context = null;
 
-  // ctx.fillStyle = "red";
-  // ctx.fillRect(20, 20, 300, 200);
+if (canvas) {
+  context = canvas.getContext("2d");
+} else {
+  alert("The canvas element is not available. Drawing cannot proceed.");
 
-  ctx.fillStyle = "green";
-  ctx.fillRect(10, 10, 150, 100);
-})();
+  throw new Error("The canvas element is not available. Drawing cannot proceed.");
+}
+
+if (context) {
+// CANVAS CENTER:
+// canvas.width = canvas.clientWidth;
+// canvas.height = canvas.clientHeight;
+const canvasCenterX = canvas.width / 2;
+const canvasCenterY = canvas.height / 2;
+
+const nodes = [];
+
+context.fillStyle = "red";
+context.fillRect(canvasCenterX - 20, canvasCenterY - 20, 20, 20);
+
+context.fillStyle = "green";
+context.fillRect(canvasCenterX - 20 / 2, canvasCenterY - 20 / 2, 20, 20);
+// ctx.fillRect(5, 50, 20, 20);
+} else {
+  alert("A context plane cannot be found. Drawing cannot proceed.");
+
+  throw new Error("A context plane cannot be found. Drawing cannot proceed.");
+}
